1805ba43bd92d369b39af90eb530d53ae3c5359a
ZigTag <17077422+ZigTag@users.noreply.github.com>
2020-12-06 01:38:48 -0800

    It now downloads and unzips a32nx to the community folder

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,11 +6,15 @@
 mod cmd;
 
 use std::{
-    fs::File,
-    io::prelude::*,
+    path,
+    fs,
+    io::{self, Write},
+    thread,
 };
 
 use serde::Serialize;
+
+use walkdir::WalkDir;
 
 #[derive(Serialize)]
 struct Reply {
@@ -48,12 +52,100 @@ fn main() {
                 Ok(command) => {
                     match command {
                         DownloadA32NX => {
-                            std::thread::spawn(move || {
+                            thread::spawn(move || {
                                 println!("Downloading");
-                                let resp = reqwest::blocking::get("https://flybywiresim-packages.nyc3.cdn.digitaloceanspaces.com/vmaster/A32NX-master.zip").unwrap();
 
-                                let mut file = File::create("A32NX-Master.zip").unwrap();
-                                file.write_all(&resp.bytes().unwrap()).unwrap();
+                                let mut search_path: Option<path::PathBuf> = None;
+
+                                let mut steam_search_path = dirs::data_dir().unwrap();
+                                steam_search_path.push("Microsoft Flight Simulator");
+                                steam_search_path.push("UserCfg.opt");
+
+                                let mut ms_store_search_path = dirs::data_local_dir().unwrap();
+                                ms_store_search_path.push("Packages");
+                                ms_store_search_path.push("Microsoft.FlightSimulator_8wekyb3d8bbwe");
+                                ms_store_search_path.push("LocalCache");
+                                ms_store_search_path.push("UserCfg.opt");
+
+                                if steam_search_path.exists() {
+                                    search_path = Some(steam_search_path);
+                                } else if ms_store_search_path.exists() {
+                                    search_path = Some(ms_store_search_path);
+                                } else {
+                                    let walker = WalkDir::new(dirs::data_dir().unwrap().parent().unwrap());
+
+                                    for i in walker.into_iter().filter_map(|e| {
+                                        match e {
+                                            Ok(e) => {
+                                                if e.path().to_str().unwrap().contains("Flight") && e.path().to_str().unwrap().contains("UserCfg.opt") {
+                                                    Some(e)
+                                                } else {
+                                                    None
+                                                }
+                                            }
+                                            Err(_) => None,
+                                        }
+                                    }) {
+                                        search_path = Some(path::PathBuf::from(i.path().to_str().unwrap()));
+                                        break;
+                                    };
+                                }
+
+                                println!("{:?}", &search_path);
+
+                                match search_path {
+                                    None => println!("MSFS not found"),
+                                    Some(_) => {
+                                        let contents = fs::read_to_string(search_path.unwrap()).unwrap();
+
+                                        for i in contents.lines() {
+                                            if i.contains("InstalledPackagesPath") {
+                                                let split_line: Vec<&str> = i.split(" ").collect();
+                                                let msfs_community_path = split_line[1];
+
+
+                                                // Strips the quotes off the path so its parsed correctly
+                                                let mut dir = path::PathBuf::from(&msfs_community_path[1..msfs_community_path.len() - 1]);
+                                                dir.push("Community");
+                                                println!("{:?}", dir);
+
+                                                if dir.exists() {
+                                                    let resp = reqwest::blocking::get("https://flybywiresim-packages.nyc3.cdn.digitaloceanspaces.com/vmaster/A32NX-master.zip").unwrap();
+
+                                                    let mut file = fs::File::create("a32nx-temp.zip").unwrap();
+                                                    file.write_all(&resp.bytes().unwrap()).unwrap();
+                                                    drop(file);
+
+                                                    let file = fs::File::open("a32nx-temp.zip").unwrap();
+                                                    let mut a32nx_zip = zip::read::ZipArchive::new(file).expect("Failed to read zip");
+
+                                                    for i in 0..a32nx_zip.len() {
+                                                        let mut file = a32nx_zip.by_index(i).unwrap();
+                                                        let out_path: path::PathBuf = [&dir.as_path(), &path::Path::new(file.name())].iter().collect();
+
+                                                        println!("{:?}", out_path);
+
+                                                        if file.is_dir() {
+                                                            fs::create_dir_all(out_path).unwrap();
+                                                        } else {
+                                                            if let Some(p) = out_path.parent() {
+                                                                if !p.exists() {
+                                                                    fs::create_dir_all(&p).unwrap();
+                                                                }
+                                                            }
+                                                            let mut out_file = fs::File::create(out_path).unwrap();
+                                                            io::copy(&mut file, &mut out_file).unwrap();
+                                                        }
+                                                    }
+
+                                                    println!("Finished Unzipping");
+
+                                                    fs::remove_file("a32nx-temp.zip").unwrap();
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
                             });
                         }
                     }
